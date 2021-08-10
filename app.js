d6388829abfdee34fbd97308c8ce9cd799b600a2
Jaimewill0511 <67603735+Jaimewill0511@users.noreply.github.com>
2021-08-10 11:56:40 +0100

    update app.js

--- app.js
+++ app.js
@@ -21,6 +21,6 @@ app.use(express.static("public"));
 app.use(Routes);
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("The Server has started on port 3000");
 });
